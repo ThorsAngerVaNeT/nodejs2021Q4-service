8d2b29709f652578fe505301f8493e150df51189
Vadzim Antonau <vanet@tut.by>
2021-12-03 20:24:00 +0300

    feat: add schema req validation, fix api.yaml for test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
-const fastify = require('fastify');
 const path = require('path');
+const httpConstants = require('http2').constants;
+const fastify = require('fastify');
+const inputValidation = require('openapi-validator-middleware');
 
 const app = fastify({ logger: { prettyPrint: true } });
+
+inputValidation.init(path.join(__dirname, '../doc/api.yaml'), {
+  framework: 'fastify',
+});
+
+app.register(inputValidation.validate());
+
+app.setErrorHandler(async (err, req, reply) => {
+  if (err instanceof inputValidation.InputValidationError) {
+    return reply
+      .status(httpConstants.HTTP_STATUS_BAD_REQUEST)
+      .send({ more_info: err.errors });
+  }
+
+  return reply
+    .status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+    .send(err.message);
+});
 
 app.register(require('fastify-swagger'), {
   mode: 'static',
