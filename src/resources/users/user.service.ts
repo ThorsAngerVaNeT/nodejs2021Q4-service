a4adec2b00d5c0fba0ee01d0d73cfdef16277d56
Vadzim Antonau <vanetthor@gmail.com>
2021-12-08 17:06:40 +0300

    docs: add TSDoc for user.service

--- src/resources/users/user.service.ts
+++ src/resources/users/user.service.ts
@@ -1,17 +1,41 @@
 import usersRepo from './user.memory.repository';
 import { User } from './user.model';
-// const tasksService = require('../tasks/task.memory.repository');
 import tasksService from '../tasks/task.service';
-// await tasksService.unassignUser(id);
 
+/**
+ * Returns all users.
+ *
+ * @returns array of all users
+ */
 const getAll = () => usersRepo.getAll();
 
+/**
+ * Returns user by id.
+ * @param id - uuid of user
+ * @returns object of user or false if not found
+ */
 const getById = (id: string) => usersRepo.getById(id);
 
+/**
+ * Create user
+ * @param user - object with name, login, password fields
+ * @returns object of new user
+ */
 const create = (user: User) => usersRepo.create(user);
 
+/**
+ * Update user by id.
+ * @param id - uuid of user
+ * @param user - object with name, login, password fields
+ * @returns object of updated user
+ */
 const update = (id: string, user: User) => usersRepo.update(id, user);
 
+/**
+ * Remove user by id and unassign user's tasks.
+ * @param id - uuid of user
+ * @returns true if user was found or false if not
+ */
 const remove = (id: string) => {
   tasksService.unassignUser(id);
   return usersRepo.remove(id);
